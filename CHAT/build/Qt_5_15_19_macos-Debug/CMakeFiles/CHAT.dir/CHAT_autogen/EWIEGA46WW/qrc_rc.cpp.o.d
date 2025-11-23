a6bcae738196203fb6c39dc88373ff8836a71833
HsiaMan <xiazhangxi@gmail.com>
2025-11-23 19:22:08 +0800

    Add .gitignore

--- CHAT/build/Qt_5_15_19_macos-Debug/CMakeFiles/CHAT.dir/CHAT_autogen/EWIEGA46WW/qrc_rc.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/CHAT.dir/CHAT_autogen/EWIEGA46WW/qrc_rc.cpp.o: \
-  /Users/zhangxihsia/Desktop/CPlusPlusFILES/CPPPROJECT/CHAT/build/Qt_5_15_19_macos-Debug/CHAT_autogen/EWIEGA46WW/qrc_rc.cpp
